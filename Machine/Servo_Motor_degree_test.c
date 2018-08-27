c2bad5fa14569ffae9e268e16e777d6ac311f332
Jay <lyj911111@naver.com>
2018-08-27 17:02:07 +0900

    Corrected voltage information

--- Machine/Servo_Motor_degree_test.c
+++ Machine/Servo_Motor_degree_test.c
@@ -25,7 +25,8 @@ int main(void)
     /*
             ※ Notice
             
-            you should set the extern Voltage 7.6V for running Servo Motor
+            you should set the extern Voltage up to 7.6V for running Servo Motor
+			(The voltage level depends on the weight)
     */
     
     
