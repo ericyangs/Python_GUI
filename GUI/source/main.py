be9075d7f6f799cd626c337b0ad266888991b12a
William Woo <woo0327@outlook.com>
2018-09-05 23:51:30 +0900

    Extra files

--- GUI/source/main.py
+++ GUI/source/main.py
@@ -3,13 +3,15 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 # call ui file for making object
+# "form_class" is the class which loads the ".ui" file in given directory
 form_class = uic.loadUiType("../ui/main_window.ui")[0]
 
 # basic class for Main Window
+# "MyWindow:" class inherits 1) QmainWindow as well as 2) form_class(Multiple Inheritance)
 class MyWindow(QMainWindow, form_class):
     def __init__(self): # constructor
         super().__init__() # call super class constructor
-        self.setupUi(self) # set Ui
+        self.setupUi(self) # "setupUi()" method display the UI already loaded
 ###############################################################################
 
 
