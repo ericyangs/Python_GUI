8bb742b57e274b8f0d94e19368049ef57aaa0ee9
William <woo0327@outlook.com>
2018-09-12 13:38:32 +0900

    Substitute if-statement with switch-case syntax

--- Machine/Test_Code/Combine_MotorPWM_Bluetooth.c
+++ Machine/Test_Code/Combine_MotorPWM_Bluetooth.c
@@ -11,6 +11,10 @@
  */ 
 
 #define F_CPU 16000000UL
+#define TRUE 1
+#define FALSE 0
+#define BAUD_HIGH 115200
+#define BAUD_LOW 9600
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -20,7 +24,7 @@
 char getValue[30];
 char buffer[30];
 volatile int i = 0;
-volatile char recive_complete = 0;
+volatile char recive_complete = FALSE;
 
 ISR(USART1_RX_vect)
 {
@@ -30,7 +34,7 @@ ISR(USART1_RX_vect)
     {
         buffer[i-1] = '\0';
         i = 0;
-        recive_complete = 1;
+        recive_complete = TRUE;
     }
 
 }
@@ -46,23 +50,55 @@ int main(void)
     OCR3A = 999;            //  initial "0 degree" of Motor     0 ~ 180 degree (range 999 ~ 4999)
     ICR3 = 39999;           // (1/16)us * 8 * (1+39999) = 20000us = 20ms*/
     
-    USART0_init(9600);    //  initializing UART 0 for checking value at Monitor
-    USART1_init(9600);    //  initializing UART 1 for BlueTooth
+    USART0_init(BAUD_LOW);    //  initializing UART 0 for checking value at Monitor
+    USART1_init(BAUD_LOW);    //  initializing UART 1 for BlueTooth
     sei();                //  Interrupt Enable
     
-    while (1) 
+    while (TRUE) 
     {
-        if(recive_complete == 1)
+        if(recive_complete == TRUE)
         {
             // get value (to see the data by Serial Monitor)
             sprintf(getValue,"[0]: %c, [1]: %c, [2]: %c, [3]: %c, [4]: %c",buffer[0],buffer[1],buffer[2],buffer[3],buffer[4]);
             USART0_puts(getValue);
             USART0_puts("\n\r");
             
-            recive_complete = 0;
+            recive_complete = FALSE;
         }
         
-        
+        // led for debug
+        switch(buffer[0])
+        {
+            case '0':
+                PORTB = 0x00;
+                OCR3A = 0;
+                break;
+
+            case '1':
+                PORTB = 0x01;
+                OCR3A = 15999;
+                _delay_ms(100);
+                OCR3A = 7999;
+                break;
+
+            case '2':
+                PORTB = 0x02;
+                OCR3A = 9999;
+                break;
+            case '3':
+                PORTB = 0x04;
+                OCR3A = 15999;
+                break;
+            case '4':
+                PORTB = 0x08;
+                OCR3A = 18999;
+                break;
+            case '5':
+                PORTB = 0x08;
+                OCR3A = 24000;
+                break;
+        }
+        /*
         // LED ON Test after getting value
         if(buffer[0] == '0')
         {
@@ -96,6 +132,7 @@ int main(void)
             PORTB = 0x08;
             OCR3A = 24000;
         }
+        */
     }
 }
 
