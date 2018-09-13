97d8e3f31da305b265e4ad66485592f896466d7a
ybbro <uvinjjang@naver.com>
2018-09-14 00:45:27 +0900

    수정함

--- Machine/FINAL_Badminton_Robot.c
+++ Machine/FINAL_Badminton_Robot.c
@@ -2,8 +2,8 @@
  *  
  *  Final Combined Coding Test
  *
- *  Created: 2018-09-10
- *  Author : Jay Lee
+ *  Created: 2018-09-14
+ *  Author : Jay Lee, YB
  *
  */ 
 
@@ -13,52 +13,29 @@
 	(디버깅 테스트 필요)
 	
 	1. hit_flag 각도 튜닝 필요.
-    2. Wheels 속도 튜닝 필요.
+    2. Wheels 속도 튜닝 필요. -> 테스트 결과 최고 속도로 움직여도 충분히 제어 가능, 다만 바퀴간 동일 PWM에 회전 속도가 다르므로 조정이 필요
     3. Master/Slave 통신으로 PWM제어 실험 필요.
 	4. 부품조립 필요 (드릴, 볼트, 너트 필요)
     5. 주의사항, 바퀴 모터드라이버, 서보모터에 12V인가. 두꺼운 wire 사용할것.
-
 */
 
 
 
-#define F_CPU 16000000UL
+//#define F_CPU 16000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include "USARR.h"
+#include "USART.h"
 
 char buf[10];
 volatile int i = 0;
 volatile char recive_complete = 0;
 unsigned int cnt = 0;
-unsigned int waitTime = 0;
 volatile int hit_flag = 0;
 
-
-// define receive code from '1' ~ '9'
-enum
-{
-    up = '1',
-    down,
-    left,
-    right,
-	
-	upLeft,
-	downLeft,
-    upRight,
-    downRight,
-    
-    hit
-
-/*
-    // Clockwise Rotation, Counter Clockwise Rotation
-    ClkWise_Rotate,
-    countor_ClkWise_Rotate,
-*/ 
-    
-};
+unsigned int stop_cnt = 0;
+volatile int stop_flag = 0;
 
 void ControlMotor(void);
 
@@ -75,57 +52,64 @@ ISR(USART1_RX_vect)
     }
 }
 
-// Interrupt per 0.1ms
+// Interrupt per 0.5ms
 ISR(TIMER0_COMP_vect)
 {
 	cnt++;
+	
 	// 0 degree of the hitting machine
 	
     if(hit_flag == 0)
     {
         if(cnt >= 7)    //  <<== Need Tuning value ***
         {
-            PORTB &= ~(1 << PB3);
+            PORTE &= ~(1 << PE0);
         }
         else
         {
-            PORTB |= (1 << PB3);
+            PORTE |= (1 << PE0);
         }
     }
     
 	//	180 degree of the hitting machine
 	if(hit_flag == 1)
 	{
-		waitTime++;
-        if(cnt >= 26)   // <<== Need Tuning value ***
+		if(cnt >= 26)   // <<== Need Tuning value ***
 		{
-			PORTB &= ~(1 << PB3);
+			PORTE &= ~(1 << PE0);
 		}
 		else
 		{
-			PORTB |= (1 << PB3);
+			PORTE |= (1 << PE0);
 		}
-        
-        if(waitTime == 10000)  //  0.1 * 10000 = 1000ms = 1s
-        {
-            waitTime = 0;
-            hit_flag = 0;
-        }            
 	}
 	// 0.1 * 200 = 20ms
 	if(cnt == 200)
 		cnt = 0;
-    
+
+/*	if (stop_flag == 1) // 1 ~ 9 사이의 컨트롤 입력이 UART 통신을 통해 들어간 후에
+	{
+		if (++stop_cnt == 200) // 1초가 지나면
+		{
+			stop_flag = 0; // 플래그를 0으로 초기화 -> 이 카운터 구문의 동작을 멈춤
+			stop_cnt = 0; // 카운터 변수를 초기화
+			buf[0] = '0'; // buf[0]에 0을 집어넣고
+			recive_complete = 1; // 송신이 완료되었다는 변수를 이용하여  while 문의 조건을 만족시켜 buf[0] = '0' 로 모든 바퀴를 정지
+		}
+	}
+    */
 }
 
 int main(void)
 {
     // For controlling the wheels of forward or backward by all of Port C
+	USART1_init(9600);
+
 	DDRC = 0xFF;
     
 	// For frequency of Hitting machine using Timer/counter 0 Interrupt
-	DDRB |= (1 << PB3);
-	//PORTB |= (1 << PB3);	//	default 'HIGH'
+	DDRE |= (1 << PE0);
+	//PORTE |= (1 << PE0\);	//	default 'HIGH'
 					
     //Timer/counter0 interrupt for Servo motor
     // CTC mode, 8 prescaler, to make 0.1ms
@@ -153,8 +137,6 @@ int main(void)
     OCR3C = 0;
     ICR3 = 1999;
     
-	USART1_init(9600);
-	
     // Global Interrupt Enable
     sei();
     
@@ -170,81 +152,86 @@ int main(void)
 
 void ControlMotor(void)
 {
-    if (buf[0] >= '1' && buf[0] <= '4') // a~f 사이의 문자가 시리얼로 들어올 경우 -> 모든 모터를 구동해야 제어가 가능하므로 하나로 묶음
-    {
-        OCR1B = 1999; // 1차 테스트 : 최고 속도로 회전시켜 사용자가 제어가 가능한지 확인해보기
-        OCR1C = 1999;
-        
-        OCR3B = 1999;
-        OCR3C = 1999;
+	if (buf[0] >= '1' && buf[0] <= '9')
+	{
+		stop_flag = 1;
+		// 여기서부터 시간을 세어 일정 시간이 지난후에 buf[0]를 0으로 바꾸어 정지하게 함 -> 만약 계속 조작하여 1~9 사이의 신호가 계속 들어오고 있다면 0이 바로 해당 신호로 바뀌게 됨
 
-        // 아래에는 각 바퀴의 방향 제어만
-        
-        if (buf[0] == up)      // ↑
-        PORTC = 0x55;           // 모두 앞방향으로 회전
+	if (buf[0] >= '1' && buf[0] <= '4') // a~f 사이의 문자가 시리얼로 들어올 경우 -> 모든 모터를 구동해야 제어가 가능하므로 하나로 묶음
+	{
+			OCR1B = 1999; // 1차 테스트 : 최고 속도로 회전시켜 사용자가 제어가 가능한지 확인해보기
+			OCR1C = 1999;
 
-        else if (buf[0] == down) // ↓
-        PORTC = 0xaa;           // 모두 뒷방향으로 회전
+			OCR3B = 1999;
+			OCR3C = 1999;
 
-        else if (buf[0] == left) // ←
-        PORTC = 0x96;           // 좌+전은 뒤로, 좌+후는 앞으로, 우+전은 앞으로, 우+후는 뒤로 회전
 
-        else if (buf[0] == right) // →
-        PORTC = 0x69;           // 좌+전은 앞으로, 좌+후는 뒤로, 우+전은 뒤로, 우+후는 앞으로 회전
-/*
-        else if (buf[0] == ClkWise_Rotate)  // 시계 방향 회전
-        PORTC = 0x5a;                       // 좌측 바퀴는 모두 앞으로, 우측 바퀴는 모두 뒤로 회전
+			// 아래에는 각 바퀴의 방향 제어만
 
-        else if (buf[0] == countor_ClkWise_Rotate)  // 반시계 방향 회전
-        PORTC = 0xa5;                               // 좌측 바퀴는 모두 뒤로, 우측 바퀴는 모두 앞으로 회전
- */   
+			if (buf[0] == '1')      // ↑
+				PORTC = 0x55;           // 모두 앞방향으로 회전
+
+			else if (buf[0] == '2') // ↓
+				PORTC = 0xaa;           // 모두 뒷방향으로 회전
+
+			else if (buf[0] == '3') // ←
+				PORTC = 0x96;           // 좌+전은 뒤로, 좌+후는 앞으로, 우+전은 앞으로, 우+후는 뒤로 회전
+
+			else if (buf[0] == '4') // →
+				PORTC = 0x69;           // 좌+전은 앞으로, 좌+후는 뒤로, 우+전은 뒤로, 우+후는 앞으로 회전
+		/*
+				else if (buf[0] == ClkWise_Rotate)  // 시계 방향 회전
+				PORTC = 0x5a;                       // 좌측 바퀴는 모두 앞으로, 우측 바퀴는 모두 뒤로 회전
+
+				else if (buf[0] == countor_ClkWise_Rotate)  // 반시계 방향 회전
+				PORTC = 0xa5;       // 좌측 바퀴는 모두 뒤로, 우측 바퀴는 모두 앞으로 회전
+		*/
+		}
+		
+
+		else if (buf[0] == '5')     // ↖
+		{
+			// 좌+후, 우+전만 앞으로 회전
+			OCR1C = 1999;
+			OCR3B = 1999;
+			PORTC = 0x14;
+		}
+
+		else if (buf[0] == '6')      // ↙
+		{
+			// 좌+전, 우+후만 뒤로 회전
+			OCR1B = 1999;
+			OCR3C = 1999;
+			PORTC = 0x82;
+		}
+
+		else if (buf[0] == '7')     // ↗
+		{
+			// 좌+전, 우+후만 앞으로 회전
+			OCR1B = 1999;
+			OCR3C = 1999;
+			PORTC = 0x41;
+		}
+
+		else if (buf[0] == '8')     // ↘
+		{
+			// 좌+후, 우+전만 뒤로 회전
+			OCR1C = 1999;
+			OCR3B = 1999;
+			PORTC = 0x28;
+		}
+		// 9는 여기다 추가
+		// 여기에 buf[0]를 날리는 코드를 추가(timer 0 쓰기)
+		_delay_ms(500);
 	}
+	else // 정해진 패킷 내의 문자가 들어오지 않거나 입력이 중단되었을 경우(0)
+	{
+		// 모든 모터의 PWM 값을 0으로 바꿔주고
+		OCR1B = 0; // PB6
+		OCR1C = 0; // PB7
+		OCR3B = 0; // PE4
+		OCR1C = 0; // PE5
 
-    else if (buf[0] == upLeft)     // ↖
-    {
-        // 좌+후, 우+전만 앞으로 회전
-        OCR1C = 1999;
-        OCR3B = 1999;
-        PORTC = 0x14;
-    }
-
-    else if(buf[0] == downLeft)      // ↙
-    {
-        // 좌+전, 우+후만 뒤로 회전
-        OCR1B = 1999;
-        OCR3C = 1999;
-        PORTC = 0x82;
-    }
-
-    else if (buf[0] == upRight)     // ↗
-    {
-        // 좌+전, 우+후만 앞으로 회전
-        OCR1B = 1999;
-        OCR3C = 1999;
-        PORTC = 0x41;
-    }
-
-    else if (buf[0] == downRight)     // ↘
-    {
-        // 좌+후, 우+전만 뒤로 회전
-        OCR1C = 1999;
-        OCR3B = 1999;
-        PORTC = 0x28;
-    }
-    else if (buf[0] == hit)
-    {
-        hit_flag = 1;
-    }
-    
- /*   else // 정해진 패킷 내의 문자가 들어오지 않거나 입력이 중단되었을 경우
-    {
-        // 모든 모터의 PWM 값을 0으로 바꿔주고
-        OCR1B = 0; // PB6
-        OCR1C = 0; // PB7
-        OCR3B = 0; // PE4
-        OCR1C = 0; // PE5
-
-        PORTC = 0;// 회전 방향을 정지로 바꿔줌
-    }
-*/
+		PORTC = 0;// 회전 방향을 정지로 바꿔줌
+	}
 }
