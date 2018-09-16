6f4455d6275415f6e7d53bce8a10df57de030f3e
Jay <lyj911111@naver.com>
2018-09-16 20:54:23 +0900

    Modified Total code for Servo motor

--- Machine/Test_Code/Servo_Motor_degree_test.c
+++ Machine/Test_Code/Servo_Motor_degree_test.c
@@ -1,9 +1,10 @@
 /*
- * PWM test.c
+ *  
+ *  Servo Motor Degree tuning of our Swing Machine.
  *
- * Created: 2018-08-13 오후 5:33:42
- * Author : Jay Lee
- 
+ *  Created: 2018-09-14
+ *  Author : Jay Lee, YB
+ *
  */ 
 
 #define F_CPU 16000000UL
@@ -11,34 +12,75 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include "USART.h"
+
+unsigned int cnt = 0;
+volatile int hit_flag = 0;
+
+// Interrupt per 0.1ms
+ISR(TIMER0_COMP_vect)
+{
+	cnt++;
+	
+	// 0 degree of the hitting machine
+    if(hit_flag == 0)
+    {
+        // Minimum value is 6. 작동할 수 있는 가장 최소값 6.
+        if(cnt >= 6)    //  <<== Need Tuning value ***
+        {
+            PORTE &= ~(1 << PE0);
+        }
+        else
+        {
+            PORTE |= (1 << PE0);
+        }
+    }
+    
+	//	180 degree of the hitting machine
+	if(hit_flag == 1)
+	{
+		// Maximum value is 24, 값이 24일때, 180도 돌아감.
+        if(cnt >= 24)   // <<== Need Tuning value ***
+		{
+			PORTE &= ~(1 << PE0);
+		}
+		else
+		{
+			PORTE |= (1 << PE0);
+		}
+	}
+	// 0.1 * 200 = 20ms
+	if(cnt == 200)
+		cnt = 0;
+
+}
 
 int main(void)
 {
-    DDRB = 0xFF;    // LED Test 
+    // For controlling the wheels of forward or backward by all of Port C
+	USART1_init(9600);
+
+	DDRC = 0xFF;
     
-    DDRE |= 0x08;           //   set PWM port at 'PE3'
-    TCCR3A = 0x82;          //   Set output Clear of 'A' of Timer/Counter 3, set Fast PWM
-    TCCR3B = 0x1A;          //  Set Prescaler 8
-    OCR3A = 999;            //  initial "0 degree" of Motor     0 ~ 180 degree (range 999 ~ 4999)
-    ICR3 = 39999;           // (1/16)us * 8 * (1+39999) = 20000us = 20ms*/
+	// For frequency of Hitting machine using Timer/counter 0 Interrupt
+	DDRE |= (1 << PE0);
+	//PORTE |= (1 << PE0\);	//	default 'HIGH'
+					
+    //Timer/counter0 interrupt for Servo motor
+    // CTC mode, 8 prescaler, to make 0.1ms
+    // (1/16)us * 8 * (1 + 199) = 100us = 0.1ms
+    TCCR0 = 0x0A;
+    OCR0 = 199;
+    TIMSK = 0x02;   //  Timer/Counter0 Comparative match interrupt enable
     
-    /*
-            ※ Notice
-            
-            you should set the extern Voltage 14V for running Servo Motor.
-			It's current flowed up to 2A, so you need large scale battery as well.
-    */
-    
+    // Global Interrupt Enable
+    sei();
     
     while (1) 
     {
-      
-        PORTB = 0xFF;           //  Turn on LED @ 0 degree
-        OCR3A = 1140;           //  value: 1140~1790  (0~180 degree),  value : 1150 ~ 1900 (0~230 degree) 7.6V설정.
-        _delay_ms(5000);
-        PORTB = 0x00;           //  Turn off LED @ target degree
-        OCR3A = 1790;           //  
-        _delay_ms(5000);
+      hit_flag = 1;
+      _delay_ms(2000);
+      hit_flag = 0;
+      _delay_ms(2000);
     }
 }
-
